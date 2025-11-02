484b655cca357745653e401b4066c3c01cab72a5
Brandon Lee <119604447+brandonleehs@users.noreply.github.com>
2025-11-02 22:14:21 +0100

    refactor: use signal for state instead

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.scss',
 })
 export class Navbar {
-  open = false;
+  open = signal(false);
 
   toggleMenu() {
-    this.open = !this.open;
+    this.open.update((isOpen) => !isOpen);
   }
 }
