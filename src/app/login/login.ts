c48c676ffcf236761ee4a7ead34419385f463f1f
Brandon Lee <119604447+brandonleehs@users.noreply.github.com>
2025-11-05 01:09:54 +0100

    feat: add login ui

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  FormsModule,
+} from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    this.validatePassword(),
+  ]);
 
+  validatePassword(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value;
+      if (!value) return null;
+      const hasUpperCase = /[A-Z]/.test(value);
+      const hasLowerCase = /[a-z]/.test(value);
+      const hasNumeric = /[0-9]/.test(value);
+      const hasSpecial = /[!@#$%^&*(),.?":{}|<>]/.test(value);
+
+      const passwordValid = hasUpperCase && hasLowerCase && hasNumeric && hasSpecial;
+
+      return !passwordValid ? { passwordStrength: true } : null;
+    };
+  }
+
+  submitForm() {
+    this.email.markAsTouched();
+    this.password.markAsTouched();
+
+    if (this.email.invalid || this.password.invalid) {
+      alert('failed!');
+      return;
+    }
+
+    alert('hi');
+    // call to auth
+  }
 }
